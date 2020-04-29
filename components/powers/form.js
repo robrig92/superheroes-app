2b1e672bff682da620a2bf55fcf46a006a37521b
Roberto Padilla <robert.ipo@hotmail.com>
2020-04-28 20:59:59 -0500

    added update behavior

--- components/powers/form.js
+++ components/powers/form.js
@@ -1,14 +1,21 @@
+import { useState } from 'react'
 import { Container } from 'reactstrap'
 import { SaveButton, SimpleBackButton } from '../../components/forms/buttons'
 
+const handleOnChange = (event, power, setPower) => {
+    let name = event.target.value
 
-export default function Form({ power }) {
+    setPower({ ...power, name: name })
+}
+
+export default function Form({ power, handleSubmit }) {
+    const [ currentPower, setPower ] = useState({...power})
     return(
         <Container>
-            <form>
+            <form onSubmit={(e) => handleSubmit(e, currentPower)}>
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
-                    <input className="form-control" name="name" aria-describedby="nameHelp" defaultValue={power.name || ''} />
+                    <input className="form-control" name="name" aria-describedby="nameHelp" value={currentPower.name || ''} onChange={e => handleOnChange(e, currentPower, setPower)} />
                     <small id="nameHelp" className="form-text text-muted">Name of the power</small>
                 </div>
                 <div className="form-group text-right">
--- pages/powers/edit/[id].js
+++ pages/powers/edit/[id].js
@@ -2,12 +2,26 @@ import Layout from "../../../components/layout";
 import Axios from 'axios';
 import Form from "../../../components/powers/form";
 
+const handleSubmit = (event, power) => {
+    event.preventDefault()
+
+    Axios.put(`http://localhost:3001/powers/${power.id}`, {
+            name: power.name
+        })
+        .then((response) => {
+            alert('Actualizado')
+        })
+        .catch((error) => {
+            alert('Ocurrió un error al actualizar')
+        })
+}
+
 export default function editForm( {power} ) {
     return(
         <Layout title={`Update power with id ${power.id}`} selected="powers">
             <div className="row">
                 <div className="col-12">
-                    <Form power={power}/>
+                    <Form power={power} handleSubmit={handleSubmit}/>
                 </div>
             </div>
         </Layout>
