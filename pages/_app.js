e8ae9f736d3b190ee064520614a0b6ba4d325958
Roberto Padilla <robert.ipo@hotmail.com>
2020-05-02 23:29:14 -0500

    removed popper and bootstrap js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
-if (typeof window !== "undefined") {
-    require ('../node_modules/jquery/dist/jquery')
-    require ('../node_modules/popper.js/dist/popper')
-    require ('../node_modules/bootstrap/dist/js/bootstrap')
-    require ('../node_modules/bootstrap/dist/css/bootstrap.min.css')
-}
+import '../node_modules/jquery/dist/jquery'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 export default function App({ Component, pageProps }) {
     return (
