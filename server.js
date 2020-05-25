1bcaf13dc7eef7189d4071b6524a8cf3e760565f
Roberto Padilla <robert.ipo@hotmail.com>
2020-05-25 01:43:06 -0500

    implementing path params routes

---
+++ server.js
@@ -0,0 +1,29 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true)
+        const { pathname, query } = parsedUrl
+
+        console.log(pathname)
+
+        if (pathname === '/heroes') {
+            app.render(req, res, '/heroes', query)
+        } else if(pathname === '/heroes/:id') {
+            console.log('aqui ando')
+            app.render(req, res, '/heroes/edit', query)
+        }
+         else {
+            handle(req, res, parsedUrl)
+        }
+    }).listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+    })
+})
