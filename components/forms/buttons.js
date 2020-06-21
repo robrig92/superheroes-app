dacb46f6ef1a8bcf8f634b4d18a9fac463af3449
Roberto Padilla <rob@MacBook-Air-de-Roberto.local>
2020-06-21 18:57:22 -0500

    implemented resposive sidebar menu

--- components/forms/buttons.js
+++ components/forms/buttons.js
@@ -9,30 +9,30 @@ import {
 
 export function SaveButton() {
     return (
-        <button type="submit" className="btn btn-primary btn-xs" data-toggle="tooltip" title="Save"><FaSave /></button>
+        <button type="submit" className="btn btn-primary btn-xs" data-toggle="tooltip" title="Save"><FaSave size="1.2em" /></button>
     )
 }
 
 export function SimpleBackButton({ href }) {
     return (
-        <Link href={href}><a className="btn btn-secondary ml-2 btn-xs" data-toggle="tooltip" title="Back"><FaDoorOpen /></a></Link>
+        <Link href={href}><a className="btn btn-secondary ml-2 btn-xs" data-toggle="tooltip" title="Back"><FaDoorOpen size="1.2em" /></a></Link>
     )
 }
 
 export function DeleteButton({ handleDelete, id }) {
     return (
-        <button className="btn btn-secondary ml-2 btn-xs" data-toggle="tooltip" title="Delete" onClick={e => handleDelete(id)}><FaTrashAlt /></button>
+        <button className="btn btn-secondary ml-2 btn-xs" data-toggle="tooltip" title="Delete" onClick={e => handleDelete(id)}><FaTrashAlt size="1.2em" /></button>
     )
 }
 
 export function EditButton({ href, as }) {
     return (
-        <Link href={href} as={as}><a className="btn btn-primary btn-xs" data-toggle="tooltip" title="Edit"><FaEdit /></a></Link>
+        <Link href={href} as={as}><a className="btn btn-primary btn-xs" data-toggle="tooltip" title="Edit"><FaEdit size="1.2em" /></a></Link>
     )
 }
 
 export function AddButton({ href, label }) {
     return (
-        <Link href={href}><a className="btn btn-primary ml-2 btn-xs" data-toggle="tooltip" title="Add"><FaPlus /> {label}</a></Link>
+        <Link href={href}><a className="btn btn-primary ml-2 btn-xs" data-toggle="tooltip" title="Add"><FaPlus size="1.2em" /> {label}</a></Link>
     )
 }
--- components/layout.js
+++ components/layout.js
@@ -4,9 +4,18 @@ import { Container } from "reactstrap"
 import styles from './layout.module.css'
 import CookiesManager from '../lib/cookies_manager';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import {
+    FaBars,
+    FaHome,
+    FaMask,
+    FaMagic,
+    FaSignOutAlt
+} from 'react-icons/fa';
 
 export default function Layout({ title, children, selected }) {
     const router = useRouter()
+    const [displayMenu, setDisplayMenu] = useState(true);
 
     const handleLogOut = (e) => {
         const cookiesManager = new CookiesManager()
@@ -16,34 +25,53 @@ export default function Layout({ title, children, selected }) {
         router.push('/login')
     }
 
-    console.log(selected)
+    const renderMenu = () => {
+        if (!displayMenu) {
+            return <></>
+        }
+
+        return (
+            <div className={`col-12 col-md-3 col-lg-2 ${styles.sidebarContainer}`}>
+                <nav className={styles.sidebar}>
+                    <ul className={`${styles.sidebarList}`}>
+                        <li className={`${styles.sideBarItem} ${selected === undefined ? styles.active : ''}`}>
+                            <Link href="/"><a className={styles.navLink}><FaHome size="1.5em"/> Home</a></Link>
+                        </li>
+                        <li className={`${styles.sideBarItem} ${selected === 'heroes' ? styles.active : ''}`}>
+                            <Link href="/heroes"><a className={styles.navLink}><FaMask size="1.5em"/> Heroes</a></Link>
+                        </li>
+                        <li className={`${styles.sideBarItem} ${selected === 'powers' ? styles.active : ''}`}>
+                            <Link href="/powers"><a className={styles.navLink}><FaMagic size="1.5em"/> Powers</a></Link>
+                        </li>
+                        <li className={`${styles.logOutButtonContainer}`}>
+                            <a className={`${styles.logOutButton}`} onClick={handleLogOut}><FaSignOutAlt size="1.2em"/> Log out</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
 
     return (
         <Container fluid className={styles.fullWidth}>
             <Head>
                 <title>Heroes app</title>
             </Head>
+            {displayMenu }
             <div className="row">
-                <div className={`col-12 col-md-3 col-lg-2 ${styles.sidebarContainer}`}>
-                    <nav className={styles.sidebar}>
-                        <ul className={`${styles.sidebarList}`}>
-                            <li className={`${styles.sideBarItem} ${selected === undefined ? styles.active : ''}`}>
-                                <Link href="/"><a className={styles.navLink}>Home</a></Link>
-                            </li>
-                            <li className={`${styles.sideBarItem} ${selected === 'heroes' ? styles.active : ''}`}>
-                                <Link href="/heroes"><a className={styles.navLink}>Heroes</a></Link>
-                            </li>
-                            <li className={`${styles.sideBarItem} ${selected === 'powers' ? styles.active : ''}`}>
-                                <Link href="/powers"><a className={styles.navLink}>Powers</a></Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-                <div className="col-12 col-md-9 col-lg-10 mt-3">
-                    <Container>
+                {renderMenu()}
+                <div className={displayMenu ? "col-12 col-md-9 col-lg-10 mt-3" : "col-12 mt-3"}>
+                    <Container fluid>
                         <div className="text-center">
                             <hr />
-                            <h1>{title ?? 'Home'}</h1>
+                            <div className="row">
+                                <div className="col-1">
+                                    <a onClick={(e) => setDisplayMenu(!displayMenu)} style={{cursor: 'pointer'}}> <FaBars size="1.2em"/> </a>
+                                </div>
+                                <div className="col-11">
+                                    <h1>{title ?? 'Home'}</h1>
+                                </div>
+                            </div>
                             <hr />
                         </div>
                         {children}
