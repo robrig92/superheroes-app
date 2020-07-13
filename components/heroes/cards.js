55eaf205d0ff38b8e7b62e50c6600e2ed7efdb29
Roberto Padilla <robert.ipo@hotmail.com>
2020-07-13 17:27:33 -0500

    Created the card score render into Cards component ready to use into another component

--- components/heroes/cards.js
+++ components/heroes/cards.js
@@ -1,24 +1,55 @@
+import React from 'react';
+import Rating from 'react-rating';
 import {
     EditButton,
     DeleteButton
 } from "../forms/buttons";
 
-const Cards = ({ heroes, handleDelete }) => {
+const Cards = ({ heroes, handleDelete, mode }) => {
+    const renderControl = (heroe) => {
+        if (mode !== 'admin') {
+            return <></>
+        }
+
+        return (
+            <div className="text-right mb-2">
+                <EditButton href="/heroes/edit/[id]" as={`/heroes/edit/${heroe.id}`} />
+                <DeleteButton id={heroe.id} handleDelete={handleDelete} />
+            </div>
+        )
+    }
+
+    const renderScore = (heroe) => {
+        if (mode !== 'scores') {
+            return <></>
+        }
+
+        return (
+            <div className="text-center mt-2">
+                <hr />
+                <Rating
+                    name="score"
+                    initialRating={(heroe.scores.reduce((sum, score) => sum + score.score, 0) / heroe.scores.length)}
+                    readonly
+                />
+                <hr />
+            </div>
+        )
+    }
+
     const renderCard = (heroe) => {
         const photoUrl = heroe.filePath ? heroe.filePath.replace('server/storage', 'http://localhost:3001/') : '';
         return (
             <div className="col-12 col-md-4 mb-2" key={heroe.id}>
                 <div className="card">
                     <img src={photoUrl} height="250" className="card-img-top" alt="..." />
+                    {renderScore(heroe)}
                     <div className="card-body">
                         <h5 className="card-title">{heroe.name}</h5>
                         <p className="card-text"><b>Age </b>{heroe.age}</p>
                         <p className="card-text"><b>Powers </b>{heroe.powers.map((power) => `${power.name}`).join(', ')}</p>
-                        <div className="text-right">
-                            <EditButton href="/heroes/edit/[id]" as={`/heroes/edit/${heroe.id}`} />
-                            <DeleteButton id={heroe.id} handleDelete={handleDelete} />
-                        </div>
                     </div>
+                    {renderControl(heroe)}
                 </div>
             </div>
         )
