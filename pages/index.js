0fcfaffffc41cabd995bd4fd836f115b3d3b1d12
Roberto Padilla <robert.ipo@hotmail.com>
2020-07-13 22:24:06 -0500

    Added the resource call from index scores

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import RequestHandler from '../lib/request_handler'
+import ResponseHandler from '../lib/response_handler'
 
-export default function Home() {
+export default function ScoreBoard({ heroes }) {
     return (
         <div className="row">
             <Head>
@@ -11,4 +13,22 @@ export default function Home() {
             </div>
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    let heroes = []
+
+    try {
+        const response = await RequestHandler.get('/heroes')
+        let responseHandler = new ResponseHandler(response)
+        heroes = responseHandler.data.Heroes
+    } catch(error) {
+        console.log(error)
+    }
+
+    return {
+        props: {
+            heroes
+        }
+    }
 }
