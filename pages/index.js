8bb3bb091ff319c8d971f844c345c8517a5c6b65
Roberto Padilla <robert.ipo@hotmail.com>
2020-07-13 23:03:14 -0500

    Created cards scores view and modified some styles

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
 import Head from 'next/head'
+import React from 'react';
+
+import Cards from '../components/heroes/cards';
 import RequestHandler from '../lib/request_handler'
 import ResponseHandler from '../lib/response_handler'
 
 export default function ScoreBoard({ heroes }) {
     return (
-        <div className="row">
+        <div className="container-fluid" style={{ minHeight: '100vh', minWidth: '100hw', backgroundColor: '#212121', color: '#ffffff' }}>
             <Head>
                 <title>Heroes</title>
             </Head>
-            <div className="col-12 text-center">
-                <h1>Rate my heroe!</h1>
+            <div className="row">
+                <div className="col-12 text-center">
+                    <div>
+                        <hr />
+                        <h1>Rate my heroes!</h1>
+                        <hr />
+                    </div>
+                </div>
+                <div className="col-12" style={{ color: '#000000' }}>
+                    <Cards
+                        heroes={heroes}
+                        handleDelete={() => {}}
+                        mode="scores"
+                    />
+                </div>
             </div>
         </div>
     )
@@ -21,7 +37,7 @@ export async function getServerSideProps(context) {
     try {
         const response = await RequestHandler.get('/heroes')
         let responseHandler = new ResponseHandler(response)
-        heroes = responseHandler.data.Heroes
+        heroes = responseHandler.data.heroes
     } catch(error) {
         console.log(error)
     }
